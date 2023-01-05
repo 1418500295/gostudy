c692c60c358b5efec464552ab5b2c84ea7c9f236
1418500295 <45054883+1418500295@users.noreply.github.com>
2023-01-05 11:01:59 +0800

    Create demo2.go

---
+++ src/demo2.go
@@ -0,0 +1,26 @@
+func main() {
+	cy := cyclicbarrier.New(num)
+	wg := sync.WaitGroup{}
+	wg1 := sync.WaitGroup{}
+	wg.Add(num)
+	wg1.Add(1)
+	for i := 0; i < num; i++ {
+		go func(i int) {
+			wg1.Wait()
+			fmt.Println("准备开始。。。")
+			time.Sleep(3 * time.Second)
+			//fmt.Println(time.Now().UnixMilli())
+			fmt.Printf("第%v个协程启动了\n", i)
+			err := cy.Await(context.Background())
+			if err != nil {
+				return
+			}
+			wg.Done()
+			fmt.Println("我在吃饭")
+		}(i)
+	}
+	fmt.Println("开始")
+	wg1.Done()
+	wg.Wait()
+	fmt.Println("结束")
+}

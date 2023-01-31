137029e6179b7082d43b7dc568ad767933977373
1418500295 <45054883+1418500295@users.noreply.github.com>
2023-01-31 17:20:57 +0800

    Update demo.go

--- src/demo.go
+++ src/demo.go
@@ -6,6 +6,34 @@ import (
 )
 
 func main() {
+	round := 5
+	var wg sync.WaitGroup
+	barrier := singleflight.Group{}
+	wg.Add(round)
+	for i := 0; i < round; i++ {
+		go func(i int) {
+			defer wg.Done()
+			fmt.Printf("%d发起请求\n", i)
+			// 启用10个协程模拟获取缓存操作
+			value, err, _ := barrier.Do("get_rand_int", func() (interface{}, error) {
+				fmt.Printf("%d正在运行\n", i)
+				time.Sleep(2 * time.Second)
+				fmt.Printf("%d执行完成\n", i)
+				return send()
+			})
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Printf("%d协程返回结果是: %v", i, value)
+			}
+		}(i)
+	}
+	wg.Wait()
+}
+
+
+
+func main() {
 	a()
 	
 	cy := cyclicbarrier.New(num)

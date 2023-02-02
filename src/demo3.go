4e2107d3f149c673fcd6b8368315086905fc73e9
1418500295 <45054883+1418500295@users.noreply.github.com>
2023-02-02 12:09:28 +0800

    Update and rename demo3 to demo3.go

--- src/demo3.go
+++ src/demo3.go
@@ -18,8 +18,8 @@ const num = 5
 
 var et int64
 var st int64
-var token3 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJnbGVuIiwianRpIjoiMTAzNyIsImlzcyI6IjkiLCJzdWIiOiIzIn0.5yx8ENOZ02yKJCgq2Ltrp0zpknQpRzI6yS2wAAzcDgE"
-var reqUrl = "https://test-www.vipsroom.net/api/scene/convert/chips/start/work/list"
+var token3 = "..5yx8ENOZ02yKJCgq2Ltrp0zpknQpRzI6yS2wAAzcDgE"
+var reqUrl = ""
 
 var resTime int64
 var chanResTime chan int64
@@ -55,7 +55,7 @@ var reqData = map[string]interface{}{
 	"start_work_end_time":   time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 23, 59, 59, 0, time.Now().Location()).Unix(),
 }
 var headers = map[string]string{
-	"t": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJnbGVuIiwianRpIjoiMTAzNyIsImlzcyI6IjkiLCJzdWIiOiIzIn0.5yx8ENOZ02yKJCgq2Ltrp0zpknQpRzI6yS2wAAzcDgE",
+	"t": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVC.",
 }
 
 func (requests *Requests) execute(i int, times int64) {

02c169ac2f725bac1117e6155606b0215e7774b8
1418500295 <45054883+1418500295@users.noreply.github.com>
2023-11-09 14:44:28 +0800

    Update demo2.go

--- src/demo2.go
+++ src/demo2.go
@@ -1,3 +1,35 @@
+打包exe带图标：
+1.go get(install) github.com/akavel/rsrc
+2.1、创建manifest文件
+仍然以test.go举例，假设打包文件名test.exe，ico文件名test.ico
+manifest文件名test.exe.manifest
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+    <assemblyIdentity
+    version="1.0.0.0"
+    processorArchitecture="x86"
+    name="controls"
+    type="win32">
+    </assemblyIdentity>
+
+    <dependency>
+        <dependentAssembly>
+            <assemblyIdentity
+            type="win32"
+            name="Microsoft.Windows.Common-Controls"
+            version="6.0.0.0"
+            processorArchitecture="*"
+            publicKeyToken="6595b64144ccf1df"
+            language="*">
+            </assemblyIdentity>
+        </dependentAssembly>
+    </dependency>
+</assembly>
+
+2.2 执行rsrc -manifest test.exe.manifest -ico test.ico -o test.exe.syso
+3. go build -0 指定文件名如：a.exe
+
+
 func main() {
 	cy := cyclicbarrier.New(num)
 	wg := sync.WaitGroup{}
